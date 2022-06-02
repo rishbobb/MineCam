e73ec2ab924cb82d0def1026f117c5e4eae34758
Rishabh <61441648+rishbobb@users.noreply.github.com>
2022-06-01 19:31:45 -0700

    Added final gradle tasks for compilation of components

--- minecam-webserver/src/main/java/com/rishabhroy/App.java
+++ minecam-webserver/src/main/java/com/rishabhroy/App.java
@@ -3,12 +3,12 @@ package com.rishabhroy;
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
 
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         Javalin app = Javalin.create(config -> {
-                config.addStaticFiles(args[0], Location.EXTERNAL);
-            }).start(5500);
+            config.addStaticFiles(args[0], Location.EXTERNAL);
+        }).start(5500);
     }
 }
